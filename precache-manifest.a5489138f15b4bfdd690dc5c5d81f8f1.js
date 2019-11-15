d48db3ebac5eda73e220502a452627ed901e0f79
phuocantd <phuocantd@gmail.com>
2019-11-15 13:53:00 +0700

    Updates

---
+++ precache-manifest.a5489138f15b4bfdd690dc5c5d81f8f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d576f0316e61cb2ea3cd1de05bcb5a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1efb71d3f36a28488b04",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7f85c48988a5dd03d3fb",
+    "url": "/static/js/2.865b6776.chunk.js"
+  },
+  {
+    "revision": "1efb71d3f36a28488b04",
+    "url": "/static/js/main.cb41f6e3.chunk.js"
+  },
+  {
+    "revision": "0f1dfdd76b3875f650e2",
+    "url": "/static/js/runtime-main.2d433452.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
